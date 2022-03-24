ea55384822e656ad9ed193e737d7ccc4d0ae21a4
leooresende01 <95967506+leooresende01@users.noreply.github.com>
2022-03-24 19:08:58 -0300

    Add files via upload

---
+++ interno.sh
@@ -0,0 +1,15 @@
+IP1=$(curl -s "192.168.0.1/cgi-bin-igd/netcore_get.cgi"  -u admin:1977 | tr -s "," "\n" | grep '"dmz_ip"' | cut -d '"' -f4)
+
+IP2=$(ifconfig ens32 | grep "inet " | grep -o inet............................... | cut -d' ' -f1-2 | cut -d':' -f2)
+
+CheckIP=$(echo "$IP1" | grep -c "$IP2")
+
+case $CheckIP in
+   1)echo "IP INTERNO OK"
+   ;;
+   
+   0)NADA=$(curl -s "192.168.0.1/cgi-bin-igd/netcore_set.cgi" -d "mod_name=netcore_set&dmz_enable=1&dmz_ip=$IP2&dmz_set=1" -u admin:1977)
+   echo "IP INTERNO CORRIGIDO"
+   ;;
+esac
+   
